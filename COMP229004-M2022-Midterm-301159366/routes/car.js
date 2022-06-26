c2e4b7936957093578eb435cc459ab2a004f1ffd
Fouad Ouardi <86013594+fouadsphone@users.noreply.github.com>
2022-06-25 23:12:39 -0400

    Add files via upload

---
+++ COMP229004-M2022-Midterm-301159366/routes/car.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+
+let carController = require('../controllers/car');
+
+// Helper function for guard purposes
+function requireAuth(req, res, next)
+{
+    if(!req.isAuthenticated())
+    {
+        req.session.url = req.originalUrl;
+        return res.redirect('/users/sign');
+    }
+    next();     
+
+}
+
+/* GET list of items */
+router.get('/list', carController.carList);
+
+// Route for Details
+router.get('/details/:id', carController.details);
+
+// Routers for edit
+router.get('/edit/:id',requireAuth, carController.displayEditPage);
+router.post('/edit/:id',requireAuth, carController.processEditPage);
+
+// Delete
+router.get('/delete/:id',requireAuth, carController.performDelete);
+
+/* GET Route for displaying the Add page - CREATE Operation */
+router.get('/add',requireAuth, carController.displayAddPage);
+
+/* POST Route for processing the Add page - CREATE Operation */
+router.post('/add',requireAuth, carController.processAddPage);
+
+module.exports = router;
